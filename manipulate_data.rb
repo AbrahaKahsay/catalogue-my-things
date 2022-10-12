c4ad496cabe4bc8fff5a9bcd46f979c68a52a761
Aiziji <aletv1501@gmail.com>
2022-10-12 13:22:19 -0400

    Fix linters

--- manipulate_data.rb
+++ manipulate_data.rb
@@ -2,51 +2,51 @@ require 'json'
 module PreserveData
   def preserve_games
     games_data = []
-    @games.each do | game |
+    @games.each do |game|
       games_data.push(
         {
           multiplayer: game.multiplayer,
           last_played_at: game.last_played_at,
           publish_date: game.publish_date,
           archived: game.archived,
-          genre: game.genre ? game.genre : nil,
-          author: game.author ? game.author : nil,
-          label: game.label ? game.label : nil,
-          source: game.source ? game.source : nil,
+          genre: game.genre || nil,
+          author: game.author || nil,
+          label: game.label || nil,
+          source: game.source || nil
         }
       )
-      File.write('./data/books.json',JSON.generate(games_data))
+      File.write('./data/books.json', JSON.generate(games_data))
     end
   end
 
   def preserve_authors
     authors_data = []
-    @authors.each do | author |
+    @authors.each do |author|
       authors_data.push(
         {
           first_name: author.first_name,
-          last_name: author.last_name,
+          last_name: author.last_name
         }
       )
-      File.write('./data/authors.json',JSON.generate(authors_data))
+      File.write('./data/authors.json', JSON.generate(authors_data))
     end
   end
 end
 
 module GetData
-  def get_games
-    if File.exists?('./data/books.json')
-      JSON.parse(File.read('./data/books.json')).each do |game|
-        @games << Game.new(game["publish_date"],game["multiplayer"],game["last_played_at"])
-      end
+  def fetch_games
+    return unless File.exist?('./data/games.json')
+
+    JSON.parse(File.read('./data/games.json')).each do |game|
+      @games << Game.new(game['publish_date'], game['multiplayer'] ? 'yes' : false, game['last_played_at'])
     end
   end
 
-  def get_authors
-    if File.exists?('./data/authors.json')
-      JSON.parse(File.read('./data/authors.json')).each do |author|
-        @authors << Author.new(author["first_name"],author["last_name"])
-      end
+  def fetch_authors
+    return unless File.exist?('./data/authors.json')
+
+    JSON.parse(File.read('./data/authors.json')).each do |author|
+      @authors << Author.new(author['first_name'], author['last_name'])
     end
   end
 end
