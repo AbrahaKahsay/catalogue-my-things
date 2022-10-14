1f9087f374f22a6500138df98cb31f7253788da4
wdavidcch <david.13.2121@gmail.com>
2022-10-14 10:45:03 -0400

    Add sql tables for books and labels

--- schema.sql
+++ schema.sql
@@ -21,4 +21,28 @@
   last_name TEXT,
   items TEXT[],
   PRIMARY KEY(id)
- )
+ );
+
+ -- Labels Table
+CREATE TABLE labels(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR(50),
+    color VARCHAR(50),
+    PRIMARY KEY(id)
+);
+-- Books Table
+CREATE TABLE books(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    publish_date DATE NOT NULL,
+    archived BOOLEAN,
+    cover_state VARCHAR(150),
+    publisher VARCHAR(150),
+    book_color VARCHAR(50),
+    books_id INT,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_labels FOREIGN KEY(books_id) REFERENCES labels(id)
+);
+
+-- Index to improvve performance of the relationship between labels and books
+
+CREATE INDEX book_label_idx ON books(labels_id);
